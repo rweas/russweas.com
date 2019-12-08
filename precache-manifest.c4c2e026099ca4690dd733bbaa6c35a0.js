7973c3d15634d601bbee27d340c6031f16415e90
Russell Weas <russweas@gmail.com>
2019-12-07 22:45:26 -0600

    Updates

---
+++ precache-manifest.c4c2e026099ca4690dd733bbaa6c35a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb58e122b67aed301763888e707887fa",
+    "url": "russweas.com/index.html"
+  },
+  {
+    "revision": "ded50b320e02e0776da1",
+    "url": "russweas.com/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85b8d1aeab52b975b460",
+    "url": "russweas.com/static/js/2.d1cee8ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "russweas.com/static/js/2.d1cee8ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ded50b320e02e0776da1",
+    "url": "russweas.com/static/js/main.a8d4e4bf.chunk.js"
+  },
+  {
+    "revision": "24e474b3f4e0a8022f7d",
+    "url": "russweas.com/static/js/runtime-main.a270a01b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "russweas.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
